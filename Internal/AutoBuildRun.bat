173cae40ef58cef4b62f4152185c4a70463d9db0
chillpert <dev@christianhilpert.com>
2022-09-20 11:12:35 +0200

    Generate BuildStatus with rebuild by default

--- Internal/AutoBuildRun.bat
+++ Internal/AutoBuildRun.bat
@@ -6,8 +6,10 @@ REM Author: github.com/chillpert
 REM Fetch project information
 call %SCRIPTS_PATH%/Internal/Fetch.bat
 
-REM If build_status.txt has not been generated yet, we can just launch the project
-if not exist "%SCRIPTS_PATH%\%BUILD_STATUS_FILE_NAME%" goto Run
+REM If build_status.txt does not exist, simply generate it and set to perform a rebuild
+if not exist "%SCRIPTS_PATH%\%BUILD_STATUS_FILE_NAME%" (
+    echo CleanBuild > "%SCRIPTS_PATH%\%BUILD_STATUS_FILE_NAME%"
+)
 
 REM Read the current build status
 set /p build_status=< "%SCRIPTS_PATH%\%BUILD_STATUS_FILE_NAME%"
