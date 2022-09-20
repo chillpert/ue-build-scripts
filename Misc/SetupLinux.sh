5e6dfebf1b98864472c6ed341de0ee9f857c6cf9
chillpert <dev@christianhilpert.com>
2022-09-20 12:13:07 +0200

    Do not copy git hooks on Linux

--- Misc/SetupLinux.sh
+++ Misc/SetupLinux.sh
@@ -15,8 +15,6 @@ git config include.path "../.gitalias"
 git submodule update --init --recursive
 
 # Copy Git hooks to hooks directory
-cp Scripts/Hooks/* .git/hooks
+# cp Scripts/Hooks/* .git/hooks
 
 # For building the project, Linux users are supposed to use ue4-cli (https://github.com/adamrehn/ue4cli)
-
-cd Scripts
