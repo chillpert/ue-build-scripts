f5244e5748ec8735ef67ebd190d5178facdf19dc
chillpert <dev@christianhilpert.com>
2023-07-07 11:40:04 +0200

    Add script for only uploading engine logs

---
+++ UploadEngineLog.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Author: github.com/chillpert
+
+# This script attempts to upload the engine log to a special branch in your Git repository that only includes log files.
+# Then it will generate a bug report template and automatically fill in the current commit as well as a link to the associated engine log.
+
+source "$(pwd)/Library.sh"
+
+# @NOTE: Modify this to match your desired branch to store log files.
+branchName="junk/logs"
+
+uploadEngineLogs
+
+read -p "Press ENTER to resume ..."
