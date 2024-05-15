a9907b3c5dfafd412a0e53e1f86aade847f27e4f
chillpert <dev@christianhilpert.com>
2024-05-15 21:15:11 +0200

    Add script to open terminal

---
+++ OpenTerminal.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Author: github.com/chillpert
+
+# This script helps terminal-noobies to quickly open a terminal to follow some instructions.
+
+source Internal/Library.sh
+
+cd "$UEBS_PROJECT_PATH"
+
+exec bash
